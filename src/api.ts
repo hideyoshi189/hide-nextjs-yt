32b0c626e057a69c7914c7a8836aede9cee5dead
hideyoshi <hideyoshi189@gmail.com>
2025-02-26 09:53:42 +0900

    await追加

--- src/api.ts
+++ src/api.ts
@@ -1,48 +1,49 @@
 import { Task } from './types';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
 export const getAllTodos = async (): Promise<Task[]> => {
-    const res = await fetch('http://localhost:3001/tasks', {
+    const res = await fetch(`${API_URL}/tasks`, {
         cache: "no-store",
     });
-    const todos = res.json();
+    const todos = await res.json();
 
     return todos;
 };
 
 export const addTodo = async (todo:Task): Promise<Task> => {
-    const res = await fetch('http://localhost:3001/tasks', {
+    const res = await fetch(`${API_URL}/tasks`, {
     method: "POST" ,
     headers: {
         "Content-Type": "application/json",
     },
     body: JSON.stringify(todo),
 });
-    const newTodo = res.json();
+    const newTodo = await res.json();
 
     return newTodo;
 };
 
 export const editTodo = async (id: string, newText: string): Promise<Task> => {
-    const res = await fetch(`http://localhost:3001/tasks/${id}`, {
+    const res = await fetch(`${API_URL}/tasks/${id}`, {
     method: "PUT" ,
     headers: {
         "Content-Type": "application/json",
     },
     body: JSON.stringify({text: newText}),
 });
-    const updatedTodo = res.json();
+    const updatedTodo = await res.json();
 
     return updatedTodo;
 };
 
 export const deleteTodo = async (id: string): Promise<Task> => {
-    const res = await fetch(`http://localhost:3001/tasks/${id}`, {
+    const res = await fetch(`${API_URL}/tasks/${id}`, {
     method: "DELETE" ,
     headers: {
         "Content-Type": "application/json",
     },
 });
-    const deleteTodo = res.json();
+    const deleteTodo = await res.json();
 
     return deleteTodo;
 };
